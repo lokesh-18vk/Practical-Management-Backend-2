5f2db5bd11d1ae9c4acc25772ac798033808f5a4
lokesh-18vk <lokeshpatil0018@gmail.com>
2024-11-23 15:39:52 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,14 +13,15 @@ app.use(express.json());
 app.use("/", router);
 
 dbConnect();
-  
-app.listen(PORT, () => {
-  console.log("Server is running at port:", PORT);
-});
-
 app.get("/",(req,res)=>{
   res,json({
     success:true,
     message:"Server Connected Successfully"
   })
 })
+
+  
+app.listen(PORT, () => {
+  console.log("Server is running at port:", PORT);
+});
+
